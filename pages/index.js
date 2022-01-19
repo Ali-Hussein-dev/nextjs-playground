cc54b961b3be221a1b6db098b912b9468db01bbb
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2022-01-19 15:35:47 +0100

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
-
+import Cookies from 'cookies';
 export default function Home({ env }) {
   console.log('env: ', JSON.parse(env));
+  env = JSON.parse(env);
   return (
     <div>
       <Head>
@@ -9,17 +10,21 @@ export default function Home({ env }) {
       </Head>
 
       <main className="bg-slate-800 min-h-screen text-slate-200 p-2">
-        Env variables: <pre>{env}</pre>
+        <div className="w-11/12">
+          Env variables:{' '}
+          <code className="max-w-fit">{JSON.stringify(env, 2, null)}</code>
+        </div>
       </main>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts
+export async function getServerSideProps(ctx) {
+  const setter = new Cookies(ctx.req, ctx.res);
+  const domain = process.env.VERCEL_URL;
+  console.log({ domain });
+  setter.set('auth', true, { domain });
   const env = JSON.stringify(process.env);
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
   return {
     props: {
       env,
