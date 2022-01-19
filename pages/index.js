45efc019a4d70025890ef79476fa44b8dc4aaaa5
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2022-01-19 14:31:57 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
-export default function Home() {
-  console.log('env: ', process.env);
+export default function Home({ env }) {
+  console.log('env: ', env);
   return (
     <div>
       <Head>
@@ -9,8 +9,20 @@ export default function Home() {
       </Head>
 
       <main className="bg-slate-800 min-h-screen text-slate-200 p-2">
-        Env variables {JSON.stringify(process.env, 2, null)}
+        Env variables: <pre>{JSON.stringify(env, 2, null)}</pre>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const envs = JSON.stringify(process.env);
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      env,
+    },
+  };
 }
