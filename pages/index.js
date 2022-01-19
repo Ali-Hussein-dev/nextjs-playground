d25131732468e71afdbc75f073b9ca82a89bf814
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2022-01-19 15:45:21 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Cookies from 'cookies';
 export default function Home({ env }) {
-  console.log('env: ', JSON.parse(env));
   env = JSON.parse(env);
   return (
     <div>
@@ -9,10 +8,10 @@ export default function Home({ env }) {
         <title>Demo</title>
       </Head>
 
-      <main className="bg-slate-800 min-h-screen text-slate-200 p-2">
-        <div className="w-11/12">
-          Env variables:{' '}
-          <code className="max-w-fit">{JSON.stringify(env, 2, null)}</code>
+      <main className="bg-slate-800 min-h-screen text-slate-200 p-2 overflow-scroll">
+        <div className="w-11/12 h-full">
+          Process.env:
+          <pre className="max-w-fit">{JSON.stringify(env, null, 4)}</pre>
         </div>
       </main>
     </div>
@@ -21,9 +20,13 @@ export default function Home({ env }) {
 
 export async function getServerSideProps(ctx) {
   const setter = new Cookies(ctx.req, ctx.res);
+
   const domain = process.env.VERCEL_URL;
-  console.log({ domain });
+  const vercelEnv = process.env.VERCEL_ENV;
+  console.log({ domain, vercelEnv });
+
   setter.set('auth', true, { domain });
+
   const env = JSON.stringify(process.env);
   return {
     props: {
