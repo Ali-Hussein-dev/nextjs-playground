185e1ad4b7608e495f547ac2bfaccf778d0f8a95
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2022-01-19 14:27:51 +0100

    add tw, log process.env

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  console.log('env: ', process.env);
   return (
     <div>
       <Head>
         <title>Demo</title>
       </Head>
 
-      <main>Env variables {JSON.stringify(process.env, 2, null)}</main>
+      <main className="bg-slate-800 min-h-screen text-slate-200 p-2">
+        Env variables {JSON.stringify(process.env, 2, null)}
+      </main>
     </div>
   );
 }
